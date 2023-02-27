93180e453b6e93c24fb5de18dc1437656ec97411
Dipankar Raj Upadhyaya <drupd17@gmail.com>
2023-02-27 19:08:06 +0530

    Sign in and Forgot Password options added

--- app/src/main/java/com/example/quizgame/ui/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/ui/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.quizgame.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.quizgame.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.sign_out, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+
+            R.id.logout -> {
+
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity , LoginPage :: class.java)
+                startActivity(intent)
+                Toast.makeText(applicationContext, "Successfully logged out", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+        return true
     }
 }
